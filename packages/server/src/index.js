ce410acdebbc1a5681948c54e651ed5b58dfbba7
akshayharti <akshay.r555@gmail.com>
2021-10-02 13:50:58 +1000

    Initial setup

---
+++ packages/server/src/index.js
@@ -0,0 +1,48 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
+
+const typeDefs = gql`
+type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }     
+`
+
+const books =  [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port: 3000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers)
